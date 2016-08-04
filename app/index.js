8534b2207a8fd89545b6e09c050e52b440a9397e
Sperkajugglite <sgomezcusguen@gmail.com>
2016-08-03 20:46:29 -0500

    Styles not working yet
    React works perfectly

--- app/index.js
+++ app/index.js
@@ -1,2 +1,12 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+
+var Previewer = React.createClass({
+  render: function() {
+    return (
+      <h1 > Atutus</h1>
+    )
+  }
+});
+
+ReactDOM.render(<Previewer />, document.getElementById("app"))
